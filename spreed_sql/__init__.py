8fab9f867373feb347fba87a25feff1d257d6717
Arubik <102335860+ArubikU@users.noreply.github.com>
2026-03-01 13:31:55 -0500

    Update __init__.py

--- spreed_sql/__init__.py
+++ spreed_sql/__init__.py
@@ -1,18 +1,18 @@
 from .models import (
-    Spreadsheet, Tab, Column, DataType, FormatRule,
+    Spreadsheet, Tab, Column, DataType, EditMode, FormatRule,
     Formula, Filter, SortSpec, PivotField, PivotTable, ComputedColumn,
     ForeignKey, Trigger, TriggerType,
     OnEdit, OnChange, OnSchedule, OnFormSubmit, OnThreshold,
 )
-from .builder import execute_schema
+from .builder import execute_schema, read_tab, read_all
 from .validator import validate_schema
 
 __all__ = [
-    "Spreadsheet", "Tab", "Column", "DataType", "FormatRule",
+    "Spreadsheet", "Tab", "Column", "DataType", "EditMode", "FormatRule",
     "Formula", "Filter", "SortSpec", "PivotField", "PivotTable", "ComputedColumn",
     "ForeignKey", "Trigger", "TriggerType",
     "OnEdit", "OnChange", "OnSchedule", "OnFormSubmit", "OnThreshold",
-    "execute_schema", "validate_schema",
+    "execute_schema", "read_tab", "read_all", "validate_schema",
 ]
 
 __version__ = "0.1.0"
